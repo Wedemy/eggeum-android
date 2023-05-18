88bb352acab96b8baec22688861aeed941650ffb
jisungbin <ji@sungb.in>
2023-05-18 18:15:23 +0900

    Make spotless happy

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,60 +12,60 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jlleitschuh.gradle.ktlint.KtlintExtension
 
 plugins {
-    alias(libs.plugins.kotlin.detekt)
-    alias(libs.plugins.kotlin.ktlint)
-    alias(libs.plugins.gradle.dependency.handler.extensions)
+  alias(libs.plugins.kotlin.detekt)
+  alias(libs.plugins.kotlin.ktlint)
+  alias(libs.plugins.gradle.dependency.handler.extensions)
 }
 
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
+  repositories {
+    google()
+    mavenCentral()
+  }
 
-    dependencies {
-        classpath(libs.kotlin.gradle)
-        classpath(libs.gradle.android)
-    }
+  dependencies {
+    classpath(libs.kotlin.gradle)
+    classpath(libs.gradle.android)
+  }
 }
 
 allprojects {
-    repositories {
-        google()
-        mavenCentral()
+  repositories {
+    google()
+    mavenCentral()
+  }
+
+  apply {
+    plugin(rootProject.libs.plugins.kotlin.detekt.get().pluginId)
+    plugin(rootProject.libs.plugins.kotlin.ktlint.get().pluginId)
+    plugin(rootProject.libs.plugins.gradle.dependency.handler.extensions.get().pluginId)
+  }
+
+  afterEvaluate {
+    extensions.configure<DetektExtension> {
+      parallel = true
+      buildUponDefaultConfig = true
+      toolVersion = libs.versions.kotlin.detekt.get()
+      config.setFrom(files("$rootDir/detekt-config.yml"))
     }
 
-    apply {
-        plugin(rootProject.libs.plugins.kotlin.detekt.get().pluginId)
-        plugin(rootProject.libs.plugins.kotlin.ktlint.get().pluginId)
-        plugin(rootProject.libs.plugins.gradle.dependency.handler.extensions.get().pluginId)
+    extensions.configure<KtlintExtension> {
+      version.set(rootProject.libs.versions.kotlin.ktlint.source.get())
+      android.set(true)
+      verbose.set(true)
     }
 
-    afterEvaluate {
-        extensions.configure<DetektExtension> {
-            parallel = true
-            buildUponDefaultConfig = true
-            toolVersion = libs.versions.kotlin.detekt.get()
-            config.setFrom(files("$rootDir/detekt-config.yml"))
-        }
-
-        extensions.configure<KtlintExtension> {
-            version.set(rootProject.libs.versions.kotlin.ktlint.source.get())
-            android.set(true)
-            verbose.set(true)
-        }
-
-        tasks.withType<KotlinCompile> {
-            kotlinOptions {
-                freeCompilerArgs = freeCompilerArgs + listOf(
-                    "-opt-in=kotlin.OptIn",
-                    "-opt-in=kotlin.RequiresOptIn",
-                )
-            }
-        }
+    tasks.withType<KotlinCompile> {
+      kotlinOptions {
+        freeCompilerArgs = freeCompilerArgs + listOf(
+          "-opt-in=kotlin.OptIn",
+          "-opt-in=kotlin.RequiresOptIn",
+        )
+      }
     }
+  }
 }
 
 tasks.register(name = "cleanAll", type = Delete::class) {
-    allprojects.map(Project::getBuildDir).forEach(::delete)
+  allprojects.map(Project::getBuildDir).forEach(::delete)
 }

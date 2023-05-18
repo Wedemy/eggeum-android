64add43d4e293b9f906d7744416e2666fc975f99
jisungbin <ji@sungb.in>
2023-05-18 19:29:20 +0900

    Add test dependencies

---
+++ cake/build.gradle.kts
@@ -0,0 +1,25 @@
+/*
+ * Designed and developed by Wedemy 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/Wedemy/eggeum-android/blob/main/LICENSE
+ */
+
+@file:Suppress("DSL_SCOPE_VIOLATION", "INLINE_FROM_HIGHER_PLATFORM")
+
+plugins {
+  eggeum("android-library")
+  eggeum("test-junit")
+  alias(libs.plugins.test.roborazzi)
+}
+
+android {
+  namespace = "us.wedemy.eggeum.cake"
+}
+
+dependencies {
+  testImplementations(
+    libs.test.robolectric,
+    libs.bundles.test.roborazzi,
+  )
+}

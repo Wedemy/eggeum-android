47d72cbba365c0f0245dbccbae3567f45f4cb136
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-06-09 22:37:26 +0900

    확장 함수, 클래스 추가

---
+++ onboard/src/main/java/us/wedemy/eggeum/onboard/util/SaveableMutableStateFlow.kt
@@ -0,0 +1,32 @@
+/*
+ * Designed and developed by Wedemy 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/Wedemy/eggeum-android/blob/main/LICENSE
+ */
+
+package us.wedemy.eggeum.onboard.util
+
+import androidx.lifecycle.SavedStateHandle
+import kotlinx.coroutines.flow.StateFlow
+
+class SaveableMutableStateFlow<T>(
+  private val savedStateHandle: SavedStateHandle,
+  private val key: String,
+  initialValue: T
+) {
+  private val state: StateFlow<T> = savedStateHandle.getStateFlow(key, initialValue)
+  var value: T
+    get() = state.value
+    set(value) {
+      savedStateHandle[key] = value
+    }
+
+  fun asStateFlow(): StateFlow<T> = state
+}
+
+fun <T> SavedStateHandle.getMutableStateFlow(
+  key: String,
+  initialValue: T
+): SaveableMutableStateFlow<T> =
+  SaveableMutableStateFlow(this, key, initialValue)

9dc71394212475db1129e61505f0b24adf42212b
jisungbin <ji@sungb.in>
2023-06-24 00:57:13 +0900

    android-splashscreen 지원 추가

--- app/src/main/kotlin/us/wedemy/eggeum/android/MainActivity.kt
+++ app/src/main/kotlin/us/wedemy/eggeum/android/MainActivity.kt
@@ -7,12 +7,45 @@
 
 package us.wedemy.eggeum.android
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+    installSplashScreen()
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.hello_world)
+    @Suppress("SetTextI18n")
+    setContentView(TextView(this).apply { text = "Hello, World!" })
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+      splashScreen.setOnExitAnimationListener { splashScreenView ->
+        ObjectAnimator.ofFloat(splashScreenView, View.ALPHA, 1f, 0f).run {
+          interpolator = AnticipateInterpolator()
+          duration = 200L
+          doOnEnd { splashScreenView.remove() }
+          start()
+        }
+      }
+    }
+
+    /*binding.root.viewTreeObserver.addOnPreDrawListener(
+      object : ViewTreeObserver.OnPreDrawListener {
+        override fun onPreDraw(): Boolean {
+          return if (isReady) {
+            binding.root.viewTreeObserver.removeOnPreDrawListener(this)
+            true
+          } else {
+            false
+          }
+        }
+      }
+    )*/
   }
 }

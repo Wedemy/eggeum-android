6af6f0677691cd6b9288fc5a3339bfabef3ad453
jisungbin <ji@sungb.in>
2023-07-09 17:28:48 +0900

    AndroidHiltPlugin 컨벤션 플러그인 추가

--- build-logic/src/main/kotlin/plugins.kt
+++ build-logic/src/main/kotlin/plugins.kt
@@ -10,6 +10,7 @@ internal object Plugins {
 
   const val KotlinJvm = "org.jetbrains.kotlin.jvm"
   const val KotlinAndroid = "org.jetbrains.kotlin.android"
+  const val KotlinKapt = "org.jetbrains.kotlin.kapt"
 
   const val AndroidApplication = "com.android.application"
   const val AndroidLibrary = "com.android.library"

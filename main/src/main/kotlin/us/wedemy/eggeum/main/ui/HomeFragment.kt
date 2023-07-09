658375bfebb9e684afba51e8d7dcbab7eed31b3c
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-07-10 02:10:26 +0900

    홈 화면 디자인 조정

--- main/src/main/kotlin/us/wedemy/eggeum/main/ui/HomeFragment.kt
+++ main/src/main/kotlin/us/wedemy/eggeum/main/ui/HomeFragment.kt
@@ -7,9 +7,10 @@
 
 package us.wedemy.eggeum.main.ui
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.viewModels
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.google.android.material.tabs.TabLayout
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,13 +20,10 @@ import us.wedemy.eggeum.main.ui.adapter.NewCafeAdapter
 import us.wedemy.eggeum.main.ui.adapter.NoticeAdapter
 import us.wedemy.eggeum.main.ui.item.NewCafeItem
 import us.wedemy.eggeum.main.ui.item.NoticeItem
-import us.wedemy.eggeum.main.viewmodel.HomeViewModel
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
   override fun getViewBinding() = FragmentHomeBinding.inflate(layoutInflater)
-
-  private val viewModel by viewModels<HomeViewModel>()
 
   private val newCafeAdapter by lazy {
     NewCafeAdapter { _ -> run {} }
@@ -35,6 +33,31 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     NoticeAdapter { _ -> run {} }
   }
 
+  val newCafes = listOf(
+    NewCafeItem("스타벅스 강남역신분당역사점", "서울특별시 강남구 강남대로 396"),
+    NewCafeItem("아티제 삼성타운점", "서울특별시 강남구 서초대로74길 11"),
+    NewCafeItem("스타벅스 강남R점", "서울특별시 강남구 강남대로 390"),
+  )
+  val newStudyCafes = listOf(
+    NewCafeItem("세컨드 라이브러리", "서울특별시 강남구 영동대로137길 6"),
+    NewCafeItem("데일리스터디카페 대치점", "서울특별시 강남구 삼성로58길 13"),
+    NewCafeItem("랭스터디카페 대치점", "서울특별시 강남구 도곡로 446"),
+  )
+  val newStudyRooms = listOf(
+    NewCafeItem("맥스터디 24시", "서울특별시 강남구 개포로 508"),
+    NewCafeItem("토즈 워크센터 선릉점", "서울특별시 강남구 테헤란로52길 21"),
+    NewCafeItem("옐로스톤 스터디룸", "서울특별시 강남구 강남대로94길 21"),
+  )
+
+  val notices = listOf(
+    NoticeItem("공부하기 좋은 카페 찾는 법", "23.03.01"),
+    NoticeItem("카페 평가하는 법", "23.03.01"),
+    NoticeItem("일이삼사오육칠팔구십일이삼사오육", "23.03.01"),
+    NoticeItem("공부하기 좋은 카페 찾는 법", "23.03.01"),
+    NoticeItem("카페 평가하는 법", "23.03.01"),
+    NoticeItem("일이삼사오육칠팔구십일이삼사오육", "23.03.01"),
+  )
+
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     initView()
@@ -43,37 +66,16 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
   }
 
   private fun initView() {
-    val newCafes = listOf(
-      NewCafeItem("스타벅스 강남역신분당역사점", "서울특별시 강남구 강남대로 396"),
-      NewCafeItem("아티제 삼성타운점", "서울특별시 강남구 서초대로74길 11"),
-      NewCafeItem("스타벅스 강남R점", "서울특별시 강남구 강남대로 390"),
-    )
-    val newStudyCafes = listOf(
-      NewCafeItem("세컨드 라이브러리", "서울특별시 강남구 영동대로137길 6"),
-      NewCafeItem("데일리스터디카페 대치점", "서울특별시 강남구 삼성로58길 13"),
-      NewCafeItem("랭스터디카페 대치점", "서울특별시 강남구 도곡로 446"),
-    )
-    val newStudyRooms = listOf(
-      NewCafeItem("맥스터디 24시", "서울특별시 강남구 개포로 508"),
-      NewCafeItem("토즈 워크센터 선릉점", "서울특별시 강남구 테헤란로52길 21"),
-      NewCafeItem("옐로스톤 스터디룸", "서울특별시 강남구 강남대로94길 21"),
-    )
-
-    val notices = listOf(
-      NoticeItem("공부하기 좋은 카페 찾는 법", "23.03.01"),
-      NoticeItem("카페 평가하는 법", "23.03.01"),
-      NoticeItem("일이삼사오육칠팔구십일이삼사오육", "23.03.01"),
-      NoticeItem("공부하기 좋은 카페 찾는 법", "23.03.01"),
-      NoticeItem("카페 평가하는 법", "23.03.01"),
-      NoticeItem("일이삼사오육칠팔구십일이삼사오육", "23.03.01"),
-    )
-
     with(binding) {
       rvHomeNewCafe.apply {
         setHasFixedSize(true)
         adapter = newCafeAdapter
         isNestedScrollingEnabled = false
-        addItemDecoration(DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL))
+        val colorDrawable =
+          ColorDrawable(ContextCompat.getColor(requireContext(), us.wedemy.eggeum.design.R.color.gray_300))
+        val dividerItemDecoration = DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
+        dividerItemDecoration.setDrawable(colorDrawable)
+        addItemDecoration(dividerItemDecoration)
       }
 
       rvHomeNotice.apply {
@@ -104,7 +106,11 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     }
   }
 
-  private fun initListener() {}
+  private fun initListener() {
+    //TODO 화면 전환 클릭 이벤트 리스터 구현
+  }
 
-  private fun initObserver() {}
+  private fun initObserver() {
+    //TODO 새로운 카페 목록 및 공지사항 목록 가져오기
+  }
 }

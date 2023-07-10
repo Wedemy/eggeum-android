dccd72db55329304d7dad4c5985b2e5bdc3a83f6
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-07-11 01:48:12 +0900

    검색 화면 권한 수락 시 내 위치로 지도가 이동 하도록 구현, ui 관련 디테일(교육용 다이얼로그, 버튼 이미지) 설정

--- main/src/main/kotlin/us/wedemy/eggeum/main/ui/SearchFragment.kt
+++ main/src/main/kotlin/us/wedemy/eggeum/main/ui/SearchFragment.kt
@@ -8,99 +8,132 @@
 package us.wedemy.eggeum.main.ui
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.viewModels
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraPosition
+import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.LocationTrackingMode
-import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
-import com.naver.maps.map.NaverMapOptions
 import com.naver.maps.map.OnMapReadyCallback
-import com.naver.maps.map.overlay.Marker
-import com.naver.maps.map.overlay.OverlayImage
 import com.naver.maps.map.util.FusedLocationSource
 import dagger.hilt.android.AndroidEntryPoint
 import us.wedemy.eggeum.common.ui.BaseFragment
-import us.wedemy.eggeum.design.R
+import us.wedemy.eggeum.main.R
 import us.wedemy.eggeum.main.databinding.FragmentSearchBinding
-import us.wedemy.eggeum.main.viewmodel.SearchViewModel
 
 @AndroidEntryPoint
 class SearchFragment : BaseFragment<FragmentSearchBinding>(), OnMapReadyCallback {
   override fun getViewBinding() = FragmentSearchBinding.inflate(layoutInflater)
 
-  private lateinit var locationSource: FusedLocationSource
   private lateinit var naverMap: NaverMap
+  private lateinit var fusedLocationClient: FusedLocationProviderClient
+  private val locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+  private val permissions =
+    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+  private var permissionsGranted = false
 
-  private val requestPermissionLauncher =
-    registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-      if (isGranted) {
-        // Permission is granted. Continue the action or workflow in your app.
+  private val requestMultiplePermissionsLauncher =
+    registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+      if (isPermissionsGranted()) {
+        permissionsGranted = true
       } else {
-        // Permission is not granted
         naverMap.locationTrackingMode = LocationTrackingMode.None
       }
     }
 
-  private val viewModel by viewModels<SearchViewModel>()
-
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-
-    locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
-
-    when {
-      ContextCompat.checkSelfPermission(
-        requireContext(),
-        Manifest.permission.ACCESS_FINE_LOCATION
-      ) == PackageManager.PERMISSION_GRANTED -> {
-        // You can use the API that requires the permission.
-        // proceed with your logic
-      }
-      shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
-        // Show an explanation to the user as to why your app needs the permission.
-        // proceed with requesting for permission
-      }
-      else -> {
-        // request permission
-        requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-      }
-    }
-
+    fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
     with(binding) {
       mvSearch.onCreate(savedInstanceState)
       mvSearch.getMapAsync(this@SearchFragment)
     }
-
+    checkPermission()
     initListener()
     initObserver()
   }
 
-  private fun initListener() {}
-
-  private fun initObserver() {}
   override fun onMapReady(naverMap: NaverMap) {
     this.naverMap = naverMap
-    naverMap.locationSource = locationSource
+    if (permissionsGranted) {
+      naverMap.locationTrackingMode = LocationTrackingMode.Follow
+    }
+    initNaverMap()
+  }
 
-    val options = NaverMapOptions()
-      .camera(CameraPosition(LatLng(37.566, 126.978), 10.0))
-      .mapType(NaverMap.MapType.Basic)
-      .enabledLayerGroups(NaverMap.LAYER_GROUP_BUILDING)
+  private fun isPermissionsGranted(): Boolean {
+    return permissions.all {
+      ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+  }
 
-    MapFragment.newInstance(options)
+  private fun checkPermission() {
+    if (shouldShowPermissionRationale()) showLocationPermissionRationaleDialog()
+    else requestPermissions()
+  }
 
-    val marker = Marker()
-    marker.apply {
-      // position = LatLng(naverMap.cameraPosition.target.latitude, naverMap.cameraPosition.target.longitude)
-      position = LatLng(37.566, 126.978)
-      icon = OverlayImage.fromResource(R.drawable.ic_pin_colored_24)
-      map = naverMap
+  private fun initListener() {
+    binding.fabSearchTracking.setOnClickListener {
+      naverMap.locationTrackingMode = LocationTrackingMode.Follow
+    }
+  }
+
+  private fun initObserver() {
+    //TODO 현재 보여지는 지도 내에 등록된 카페 목록 가져오기
+  }
+
+  private fun initNaverMap() {
+    naverMap.apply {
+      locationSource = this@SearchFragment.locationSource
+      uiSettings.isZoomControlEnabled = false
+      cameraPosition = CameraPosition(
+        LatLng(naverMap.cameraPosition.target.latitude, naverMap.cameraPosition.target.longitude),
+        ZOOM_LEVEL,
+      )
+      setLayerGroupEnabled(NaverMap.LAYER_GROUP_BUILDING, true)
+    }
+    moveToCameraToUserLocation()
+  }
+
+  @SuppressLint("MissingPermission")
+  private fun moveToCameraToUserLocation() {
+    fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+      val cameraUpdate = CameraUpdate.scrollTo(LatLng(location.latitude, location.longitude))
+      naverMap.moveCamera(cameraUpdate)
+    }
+  }
+
+  private fun requestPermissions() {
+    requestMultiplePermissionsLauncher.launch(permissions)
+  }
+
+  private fun shouldShowPermissionRationale(): Boolean {
+    return (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)
+      || shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION))
+  }
+
+  private fun showLocationPermissionRationaleDialog() {
+    val dialog = AlertDialog.Builder(requireContext())
+      .setMessage(getString(R.string.location_permission_educational_message))
+      .setPositiveButton(getString(R.string.check)) { _, _ ->
+        requestPermissions()
+      }
+      .setNegativeButton(getString(R.string.cancel), null)
+      .show()
+    dialog.apply {
+      getButton(DialogInterface.BUTTON_POSITIVE)
+        .setTextColor(ContextCompat.getColor(requireContext(), us.wedemy.eggeum.design.R.color.teal_500))
+      getButton(DialogInterface.BUTTON_NEGATIVE)
+        .setTextColor(ContextCompat.getColor(requireContext(), us.wedemy.eggeum.design.R.color.gray_400))
     }
   }
 
@@ -136,5 +169,6 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(), OnMapReadyCallback
 
   private companion object {
     private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+    private const val ZOOM_LEVEL = 15.0
   }
 }

2d5d4c270f2560fe9c178f32b74f518d8f95e989
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-07-25 01:57:17 +0900

    Enum 목록 조회 API 통신 로직 구현
    API 가 정상적으로 호출되는지 호출 해보기 위해 테스트
    정상적으로 호출되는 것을 확인
    Login 기능 구현 branch 에 어울리는 것 같진 않아서 관계없다 판단되면 관련 파일 다 날리겠습니다

---
+++ domain/src/main/kotlin/us/wedemy/eggeum/android/domain/repository/EnumRepository.kt
@@ -0,0 +1,18 @@
+/*
+ * Designed and developed by Wedemy 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/Wedemy/eggeum-android/blob/main/LICENSE
+ */
+
+package us.wedemy.eggeum.android.domain.repository
+
+import us.wedemy.eggeum.android.domain.model.enums.EnumList
+
+/** Enum 조회 API */
+public interface EnumRepository {
+  /**
+   * 전체 Enum 목록 조회
+   */
+  public suspend fun getEnumList(): EnumList?
+}

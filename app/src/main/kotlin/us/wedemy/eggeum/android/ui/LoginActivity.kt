c04cc92fa7e888f407b7c2697c5a90c4c6b76653
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-08-08 17:40:28 +0900

    로그인 관련 문제 해결
    googleIdToken 이 아닌 firebaseIdToken 을 서버에 전달

--- app/src/main/kotlin/us/wedemy/eggeum/android/ui/LoginActivity.kt
+++ app/src/main/kotlin/us/wedemy/eggeum/android/ui/LoginActivity.kt
@@ -19,6 +19,9 @@ import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.identity.Identity
 import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 import us.wedemy.eggeum.android.BuildConfig
@@ -37,15 +40,27 @@ class LoginActivity : BaseActivity() {
 
   private lateinit var oneTapClient: SignInClient
   private lateinit var signInRequest: BeginSignInRequest
+  private lateinit var auth: FirebaseAuth
 
   private val oneTapClientResult =
     registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
       if (result.resultCode == Activity.RESULT_OK) {
         try {
           val credential = oneTapClient.getSignInCredentialFromIntent(result.data)
-          val idToken = credential.googleIdToken
-          idToken?.let {
-            viewModel.getLoginBody(it)
+          val googleIdToken = credential.googleIdToken
+          if (googleIdToken != null) {
+            val user = FirebaseAuth.getInstance().currentUser
+            user!!.getIdToken(true)
+              .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                  val firebaseIdToken: String? = task.result.token
+                  viewModel.getLoginBody(firebaseIdToken!!)
+                } else {
+                  Timber.d("${task.exception}")
+                }
+              }
+          } else {
+            Timber.d("No ID token!")
           }
         } catch (exception: ApiException) {
           Timber.e(exception.localizedMessage)
@@ -55,6 +70,7 @@ class LoginActivity : BaseActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    auth = Firebase.auth
     initGoogleLogin()
     initListener()
   }

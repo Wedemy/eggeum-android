b6fabb16382d5d8900deb1667914ee830aa16637
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-09-06 13:30:03 +0900

    로그인 화면과 홈화면, 온보딩 화면 연동

--- app/src/main/kotlin/us/wedemy/eggeum/android/ui/LoginActivity.kt
+++ app/src/main/kotlin/us/wedemy/eggeum/android/ui/LoginActivity.kt
@@ -8,6 +8,7 @@
 package us.wedemy.eggeum.android.ui
 
 import android.app.Activity
+import android.content.Intent
 import android.content.IntentSender
 import android.graphics.Color
 import android.os.Bundle
@@ -22,10 +23,14 @@ import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import us.wedemy.eggeum.android.BuildConfig
+import us.wedemy.eggeum.android.common.extension.repeatOnStarted
 import us.wedemy.eggeum.android.common.ui.BaseActivity
 import us.wedemy.eggeum.android.databinding.ActivityLoginBinding
+import us.wedemy.eggeum.android.main.ui.MainActivity
+import us.wedemy.eggeum.android.onboard.ui.OnboardActivity
 import us.wedemy.eggeum.android.viewmodel.LoginViewModel
 
 @AndroidEntryPoint
@@ -69,6 +74,7 @@ class LoginActivity : BaseActivity() {
     super.onCreate(savedInstanceState)
     initGoogleLogin()
     initListener()
+    initObserver()
   }
 
   private fun initGoogleLogin() {
@@ -104,6 +110,23 @@ class LoginActivity : BaseActivity() {
         .addOnFailureListener(this) { exception ->
           Timber.e(exception.localizedMessage)
         }
+    }
+  }
+  private fun initObserver() {
+    repeatOnStarted {
+      launch {
+        viewModel.navigateToMainEvent.collect {
+          startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+          finish()
+        }
+      }
+
+      launch {
+        viewModel.navigateToOnBaordingEvent.collect {
+          startActivity(Intent(this@LoginActivity, OnboardActivity::class.java))
+          finish()
+        }
+      }
     }
   }
 }
--- app/src/main/kotlin/us/wedemy/eggeum/android/viewmodel/LoginViewModel.kt
+++ app/src/main/kotlin/us/wedemy/eggeum/android/viewmodel/LoginViewModel.kt
@@ -11,6 +11,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import us.wedemy.eggeum.android.domain.usecase.GetLoginBodyUseCase
@@ -19,6 +22,13 @@ import us.wedemy.eggeum.android.domain.usecase.GetLoginBodyUseCase
 class LoginViewModel @Inject constructor(
   private val getLoginBodyUseCase: GetLoginBodyUseCase,
 ) : ViewModel() {
+
+  private val _navigateToMainEvent = MutableSharedFlow<Unit>()
+  val navigateToMainEvent: SharedFlow<Unit> = _navigateToMainEvent.asSharedFlow()
+
+  private val _navigateToOnBoardingEvent = MutableSharedFlow<Unit>()
+  val navigateToOnBaordingEvent: SharedFlow<Unit> = _navigateToOnBoardingEvent.asSharedFlow()
+
   fun getLoginBody(idToken: String) {
     viewModelScope.launch {
       val result = getLoginBodyUseCase.execute(idToken)
@@ -26,6 +36,7 @@ class LoginViewModel @Inject constructor(
         result.isSuccess && result.getOrNull() != null -> {
           val loginBody = result.getOrNull()
           Timber.d("$loginBody")
+          _navigateToMainEvent.emit(Unit)
         }
         result.isSuccess && result.getOrNull() == null -> {
           Timber.e("Request succeeded but data validation failed.")
@@ -33,6 +44,8 @@ class LoginViewModel @Inject constructor(
         result.isFailure -> {
           val exception = result.exceptionOrNull()
           Timber.d(exception)
+          //TODO errorCode 가 404 일때만 온보딩 화면으로 이동
+          _navigateToOnBoardingEvent.emit(Unit)
         }
       }
     }

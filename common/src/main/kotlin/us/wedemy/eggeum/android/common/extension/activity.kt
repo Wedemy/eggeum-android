b04c56f7c1bcfd6d60fc8fc602edb86ef6db8197
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-10-08 07:03:39 +0900

    activity 확장 함수 네이밍 변경

--- common/src/main/kotlin/us/wedemy/eggeum/android/common/extension/activity.kt
+++ common/src/main/kotlin/us/wedemy/eggeum/android/common/extension/activity.kt
@@ -11,9 +11,9 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Build
 
-inline fun <reified T : Activity> Activity.changeActivityWithAnimation(
+inline fun <reified T : Activity> Activity.startActivityWithAnimation(
   intentBuilder: Intent.() -> Intent = { this },
-  finishActivity: Boolean = true,
+  withFinish: Boolean = true,
 ) {
   startActivity(Intent(this, T::class.java).intentBuilder())
   if (Build.VERSION.SDK_INT >= 34) {
@@ -26,5 +26,5 @@ inline fun <reified T : Activity> Activity.changeActivityWithAnimation(
     @Suppress("DEPRECATION")
     overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
   }
-  if (finishActivity) finish()
+  if (withFinish) finish()
 }

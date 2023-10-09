71b753530cb995f89a43ffbcf377e88de2dfa694
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-10-09 16:55:17 +0900

    view expand collapse animation 비활성화

--- common/src/main/kotlin/us/wedemy/eggeum/android/common/util/ToggleAnimation.kt
+++ common/src/main/kotlin/us/wedemy/eggeum/android/common/util/ToggleAnimation.kt
@@ -12,59 +12,65 @@ import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.Transformation
 
-object ToggleAnimation {
-  fun toggleArrow(view: View, isExpanded: Boolean): Boolean {
-    return if (isExpanded) {
-      view.animate().setDuration(200).rotation(180f)
-      true
-    } else {
-      view.animate().setDuration(200).rotation(0f)
-      false
-    }
-  }
+class ToggleAnimation {
 
-  fun expand(view: View) {
-    val animation = expandAction(view)
-    view.startAnimation(animation)
-  }
-
-  private fun expandAction(view: View): Animation {
-    view.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-    val actualHeight = view.measuredHeight
-
-    view.layoutParams.height = 0
-    view.visibility = View.VISIBLE
-
-    val animation = object : Animation() {
-      override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
-        view.apply {
-          layoutParams.height = if (interpolatedTime == 1f) ViewGroup.LayoutParams.WRAP_CONTENT
-          else (actualHeight * interpolatedTime).toInt()
-          requestLayout()
-        }
+  companion object {
+    fun toggleArrow(view: View, isExpanded: Boolean): Boolean {
+      return if (isExpanded) {
+        view.animate().setDuration(200).rotation(180f)
+        true
+      } else {
+        view.animate().setDuration(200).rotation(0f)
+        false
       }
     }
-    animation.duration = (actualHeight / view.context.resources.displayMetrics.density).toLong()
-    view.startAnimation(animation)
 
-    return animation
-  }
+    fun expand(view: View) {
+      val animation = expandAction(view)
+      view.startAnimation(animation)
+    }
 
-  fun collapse(view: View) {
-    val actualHeight = view.measuredHeight
-    val animation = object : Animation() {
-      override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
-        if (interpolatedTime == 1f) {
-          view.visibility = View.GONE
-        } else {
-          view.apply {
-            layoutParams.height = (actualHeight - (actualHeight * interpolatedTime)).toInt()
-            requestLayout()
+    private fun expandAction(view: View): Animation {
+      view.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+      val actualHeight = view.measuredHeight
+
+      view.layoutParams.height = 0
+      view.visibility = View.VISIBLE
+
+      val animation = object : Animation() {
+        override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
+          view.layoutParams.height = if (interpolatedTime == 1f) {
+            ViewGroup.LayoutParams.WRAP_CONTENT
+          } else {
+            (actualHeight * interpolatedTime).toInt()
+          }
+          view.requestLayout()
+        }
+      }
+      animation.duration = (actualHeight / view.context.resources.displayMetrics.density).toLong()
+      return animation
+    }
+
+    fun collapse(view: View) {
+      val animation = collapseAction(view)
+      view.startAnimation(animation)
+    }
+
+    private fun collapseAction(view: View): Animation {
+      val actualHeight = view.measuredHeight
+
+      val animation = object : Animation() {
+        override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
+          if (interpolatedTime == 1f) {
+            view.visibility = View.GONE
+          } else {
+            view.layoutParams.height = (actualHeight - (actualHeight * interpolatedTime)).toInt()
+            view.requestLayout()
           }
         }
       }
+      animation.duration = (actualHeight / view.context.resources.displayMetrics.density).toLong()
+      return animation
     }
-    animation.duration = (actualHeight / view.context.resources.displayMetrics.density).toLong()
-    view.startAnimation(animation)
   }
 }
--- main/src/main/kotlin/us/wedemy/eggeum/android/main/ui/adapter/viewholder/NoticeListItemViewHolder.kt
+++ main/src/main/kotlin/us/wedemy/eggeum/android/main/ui/adapter/viewholder/NoticeListItemViewHolder.kt
@@ -8,9 +8,7 @@
 package us.wedemy.eggeum.android.main.ui.adapter.viewholder
 
 import android.view.View
-import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
-import us.wedemy.eggeum.android.common.util.ToggleAnimation
 import us.wedemy.eggeum.android.main.databinding.ItemNoticeListBinding
 import us.wedemy.eggeum.android.main.ui.item.NoticeItem
 
@@ -20,20 +18,70 @@ class NoticeListItemViewHolder(val binding: ItemNoticeListBinding) : RecyclerVie
       tvNoticeTitle.text = notice.title
       tvNoticeDate.text = notice.date
       tvNoticeDescription.text = notice.description
+//      clNotice.setOnClickListener {
+//        val show = toggleLayout(!notice.isExpanded, ivNoticeExpand, llLayoutExpand)
+//        notice.isExpanded = show
+//      }
       clNotice.setOnClickListener {
-        val show = toggleLayout(!notice.isExpanded, ivNoticeExpand, llLayoutExpand)
-        notice.isExpanded = show
+        toggleContent(binding.llLayoutExpand)
       }
     }
   }
 
-  private fun toggleLayout(isExpanded: Boolean, view: View, layoutExpand: LinearLayout): Boolean {
-    ToggleAnimation.toggleArrow(view, isExpanded)
-    if (isExpanded) {
-      ToggleAnimation.expand(layoutExpand)
+//  private fun toggleLayout(isExpanded: Boolean, view: View, layoutExpand: LinearLayout): Boolean {
+//    ToggleAnimation.toggleArrow(view, isExpanded)
+//    if (isExpanded) {
+//      ToggleAnimation.expand(layoutExpand)
+//    } else {
+//      ToggleAnimation.collapse(layoutExpand)
+//    }
+//    return isExpanded
+//  }
+
+  private fun toggleContent(hiddenView: View) {
+    if (hiddenView.visibility == View.VISIBLE) {
+      // collapseView(hiddenView)
+      with(binding) {
+        llLayoutExpand.visibility = View.GONE
+        ivNoticeExpand.animate().setDuration(200).rotation(180f)
+      }
     } else {
-      ToggleAnimation.collapse(layoutExpand)
+      // expandView(hiddenView)
+      with(binding) {
+        llLayoutExpand.visibility = View.VISIBLE
+        ivNoticeExpand.animate().setDuration(200).rotation(0f)
+      }
     }
-    return isExpanded
   }
+
+//  private fun expandView(view: View) {
+//    view.measure(RecyclerView.LayoutParams.MATCH_PARENT, RecyclerView.LayoutParams.WRAP_CONTENT)
+//    val targetHeight = view.measuredHeight
+//
+//    view.layoutParams.height = 0
+//    view.visibility = View.VISIBLE
+//    val animator = ValueAnimator.ofInt(0, targetHeight)
+//    animator.addUpdateListener { animation ->
+//      view.layoutParams.height = (animation.animatedValue as Int)
+//      view.requestLayout()
+//    }
+//    animator.duration = 300
+//    animator.start()
+//  }
+//
+//  private fun collapseView(view: View) {
+//    val initialHeight = view.measuredHeight
+//    val animator = ValueAnimator.ofInt(initialHeight, 0)
+//    animator.addUpdateListener { animation ->
+//      view.layoutParams.height = (animation.animatedValue as Int)
+//      view.requestLayout()
+//    }
+//    animator.addListener(object : AnimatorListenerAdapter() {
+//      override fun onAnimationEnd(animation: Animator) {
+//        view.visibility = View.GONE
+//      }
+//    })
+//    animator.duration = 300
+//    animator.start()
+//  }
 }

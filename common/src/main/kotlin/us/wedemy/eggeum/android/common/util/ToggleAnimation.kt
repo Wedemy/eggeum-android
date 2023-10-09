e33393d8495c0cc8da0fe4b137249fc2a77a298c
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-10-09 17:00:48 +0900

    style check success

--- common/src/main/kotlin/us/wedemy/eggeum/android/common/util/ToggleAnimation.kt
+++ common/src/main/kotlin/us/wedemy/eggeum/android/common/util/ToggleAnimation.kt
@@ -12,65 +12,63 @@ import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.Transformation
 
-class ToggleAnimation {
+object ToggleAnimation {
 
-  companion object {
-    fun toggleArrow(view: View, isExpanded: Boolean): Boolean {
-      return if (isExpanded) {
-        view.animate().setDuration(200).rotation(180f)
-        true
-      } else {
-        view.animate().setDuration(200).rotation(0f)
-        false
+  fun toggleArrow(view: View, isExpanded: Boolean): Boolean {
+    return if (isExpanded) {
+      view.animate().setDuration(200).rotation(180f)
+      true
+    } else {
+      view.animate().setDuration(200).rotation(0f)
+      false
+    }
+  }
+
+  fun expand(view: View) {
+    val animation = expandAction(view)
+    view.startAnimation(animation)
+  }
+
+  private fun expandAction(view: View): Animation {
+    view.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+    val actualHeight = view.measuredHeight
+
+    view.layoutParams.height = 0
+    view.visibility = View.VISIBLE
+
+    val animation = object : Animation() {
+      override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
+        view.layoutParams.height = if (interpolatedTime == 1f) {
+          ViewGroup.LayoutParams.WRAP_CONTENT
+        } else {
+          (actualHeight * interpolatedTime).toInt()
+        }
+        view.requestLayout()
       }
     }
+    animation.duration = (actualHeight / view.context.resources.displayMetrics.density).toLong()
+    return animation
+  }
 
-    fun expand(view: View) {
-      val animation = expandAction(view)
-      view.startAnimation(animation)
-    }
+  fun collapse(view: View) {
+    val animation = collapseAction(view)
+    view.startAnimation(animation)
+  }
 
-    private fun expandAction(view: View): Animation {
-      view.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-      val actualHeight = view.measuredHeight
+  private fun collapseAction(view: View): Animation {
+    val actualHeight = view.measuredHeight
 
-      view.layoutParams.height = 0
-      view.visibility = View.VISIBLE
-
-      val animation = object : Animation() {
-        override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
-          view.layoutParams.height = if (interpolatedTime == 1f) {
-            ViewGroup.LayoutParams.WRAP_CONTENT
-          } else {
-            (actualHeight * interpolatedTime).toInt()
-          }
+    val animation = object : Animation() {
+      override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
+        if (interpolatedTime == 1f) {
+          view.visibility = View.GONE
+        } else {
+          view.layoutParams.height = (actualHeight - (actualHeight * interpolatedTime)).toInt()
           view.requestLayout()
         }
       }
-      animation.duration = (actualHeight / view.context.resources.displayMetrics.density).toLong()
-      return animation
     }
-
-    fun collapse(view: View) {
-      val animation = collapseAction(view)
-      view.startAnimation(animation)
-    }
-
-    private fun collapseAction(view: View): Animation {
-      val actualHeight = view.measuredHeight
-
-      val animation = object : Animation() {
-        override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
-          if (interpolatedTime == 1f) {
-            view.visibility = View.GONE
-          } else {
-            view.layoutParams.height = (actualHeight - (actualHeight * interpolatedTime)).toInt()
-            view.requestLayout()
-          }
-        }
-      }
-      animation.duration = (actualHeight / view.context.resources.displayMetrics.density).toLong()
-      return animation
-    }
+    animation.duration = (actualHeight / view.context.resources.displayMetrics.density).toLong()
+    return animation
   }
 }

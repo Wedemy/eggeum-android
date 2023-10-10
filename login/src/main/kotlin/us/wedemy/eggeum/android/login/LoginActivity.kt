7e7f1847cfa266b60af8c0ff54bb95786a0c8e36
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-10-10 13:28:40 +0900

    개발의 편의성을 위해 기존 구글 로그인 방식으로 migration
    원탭 로그인은 에뮬레이터에서 로그인이 되지 않는 문제가 있고, block 될 수 있기 때문에 개발 하는데 영향을 주고 있어, 이전 로그인 방식으로 변경

--- login/src/main/kotlin/us/wedemy/eggeum/android/login/LoginActivity.kt
+++ login/src/main/kotlin/us/wedemy/eggeum/android/login/LoginActivity.kt
@@ -8,18 +8,22 @@
 package us.wedemy.eggeum.android.login
 
 import android.app.Activity
-import android.content.IntentSender
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.SystemBarStyle
-import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.Scopes
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.Scope
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -49,45 +53,48 @@ class LoginActivity : BaseActivity() {
   @Inject
   lateinit var onboardNavigator: OnboardNavigator
 
-  private lateinit var oneTapClient: SignInClient
-  private lateinit var signInRequest: BeginSignInRequest
+//  private lateinit var oneTapClient: SignInClient
+//  private lateinit var signInRequest: BeginSignInRequest
   private lateinit var idToken: String
 
+  private lateinit var googleSignInClient: GoogleSignInClient
+  private lateinit var loginLauncher: ActivityResultLauncher<Intent>
+
   // TODO 네트워크 연결 문제 관련 토스트 출력
-  private val oneTapClientResult =
-    registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
-      if (result.resultCode == Activity.RESULT_OK) {
-        try {
-          // 인텐트로 부터 로그인 자격 정보를 가져옴
-          val credential = oneTapClient.getSignInCredentialFromIntent(result.data)
-          // 가져온 자격 증명에서 Google ID 토큰을 추출
-          val googleIdToken = credential.googleIdToken
-          if (googleIdToken != null) {
-            // Google ID 토큰을 사용해 Firebase 인증 자격 증명을 생성
-            val firebaseCredential = GoogleAuthProvider.getCredential(googleIdToken, null)
-            // 생성된 Firebase 인증 자격 증명을 사용하여 Firebase 에 로그인을 시도
-            Firebase.auth.signInWithCredential(firebaseCredential).addOnCompleteListener { task ->
-              if (task.isSuccessful) {
-                Firebase.auth.currentUser?.getIdToken(true)?.addOnCompleteListener { idTokenTask ->
-                  if (idTokenTask.isSuccessful) {
-                    idTokenTask.result?.token?.let { token ->
-                      idToken = token
-                      viewModel.login(idToken)
-                    } ?: Timber.e("FirebaseIdToken is null.")
-                  }
-                }
-              } else {
-                Timber.e(task.exception)
-              }
-            }
-          } else {
-            Timber.e("GoogleIdToken is null.")
-          }
-        } catch (exception: ApiException) {
-          Timber.e(exception.localizedMessage)
-        }
-      }
-    }
+//  private val oneTapClientResult =
+//    registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
+//      if (result.resultCode == Activity.RESULT_OK) {
+//        try {
+//          // 인텐트로 부터 로그인 자격 정보를 가져옴
+//          val credential = oneTapClient.getSignInCredentialFromIntent(result.data)
+//          // 가져온 자격 증명에서 Google ID 토큰을 추출
+//          val googleIdToken = credential.googleIdToken
+//          if (googleIdToken != null) {
+//            // Google ID 토큰을 사용해 Firebase 인증 자격 증명을 생성
+//            val firebaseCredential = GoogleAuthProvider.getCredential(googleIdToken, null)
+//            // 생성된 Firebase 인증 자격 증명을 사용하여 Firebase 에 로그인을 시도
+//            Firebase.auth.signInWithCredential(firebaseCredential).addOnCompleteListener { task ->
+//              if (task.isSuccessful) {
+//                Firebase.auth.currentUser?.getIdToken(true)?.addOnCompleteListener { idTokenTask ->
+//                  if (idTokenTask.isSuccessful) {
+//                    idTokenTask.result?.token?.let { token ->
+//                      idToken = token
+//                      viewModel.login(idToken)
+//                    } ?: Timber.e("FirebaseIdToken is null.")
+//                  }
+//                }
+//              } else {
+//                Timber.e(task.exception)
+//              }
+//            }
+//          } else {
+//            Timber.e("GoogleIdToken is null.")
+//          }
+//        } catch (exception: ApiException) {
+//          Timber.e(exception.localizedMessage)
+//        }
+//      }
+//    }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -98,37 +105,56 @@ class LoginActivity : BaseActivity() {
 
   private fun initGoogleLogin() {
     // 현쟈 액티비티(this)에 대한 Google One Tap 로그인 클라이언트를 가져옴
-    oneTapClient = Identity.getSignInClient(this)
-    signInRequest = BeginSignInRequest.builder()
-      .setGoogleIdTokenRequestOptions(
-        BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-          // Google Id Token 기반 로그인을 지원하도록 설정
-          .setSupported(true)
-          // 서버의 클라이언트 ID 를 설정
-          .setServerClientId(BuildConfig.GOOGLE_CLIENT_ID)
-          // 기존에 인증된 계정만을 필터링하지 않도록 설정
-          .setFilterByAuthorizedAccounts(false)
-          .build(),
-      )
-      // 이전에 선택 했던 계정을 기억
-      .setAutoSelectEnabled(true)
+//    oneTapClient = Identity.getSignInClient(this)
+//    signInRequest = BeginSignInRequest.builder()
+//      .setGoogleIdTokenRequestOptions(
+//        BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+//          // Google Id Token 기반 로그인을 지원하도록 설정
+//          .setSupported(true)
+//          // 서버의 클라이언트 ID 를 설정
+//          .setServerClientId(BuildConfig.GOOGLE_CLIENT_ID)
+//          // 기존에 인증된 계정만을 필터링하지 않도록 설정
+//          .setFilterByAuthorizedAccounts(false)
+//          .build(),
+//      )
+//      // 이전에 선택 했던 계정을 기억
+//      .setAutoSelectEnabled(true)
+//      .build()
+
+    val googleClientId = BuildConfig.GOOGLE_CLIENT_ID
+    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+      .requestScopes(Scope(Scopes.DRIVE_APPFOLDER))
+      .requestIdToken(googleClientId)
+      .requestServerAuthCode(googleClientId)
+      .requestEmail()
       .build()
+
+    loginLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+      if (result.resultCode == Activity.RESULT_OK) {
+        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+        handleSignInResult(task)
+      }
+    }
+
+    googleSignInClient = GoogleSignIn.getClient(this, gso)
   }
 
   private fun initListener() {
     binding.cvGoogleLogin.setOnSingleClickListener {
-      oneTapClient
-        .beginSignIn(signInRequest)
-        .addOnSuccessListener(this) { result ->
-          try {
-            oneTapClientResult.launch(IntentSenderRequest.Builder(result.pendingIntent.intentSender).build())
-          } catch (exception: IntentSender.SendIntentException) {
-            Timber.e("Couldn't start One Tap UI: ${exception.localizedMessage}")
-          }
-        }
-        .addOnFailureListener(this) { exception ->
-          Timber.e(exception.localizedMessage)
-        }
+//      oneTapClient
+//        .beginSignIn(signInRequest)
+//        .addOnSuccessListener(this) { result ->
+//          try {
+//            oneTapClientResult.launch(IntentSenderRequest.Builder(result.pendingIntent.intentSender).build())
+//          } catch (exception: IntentSender.SendIntentException) {
+//            Timber.e("Couldn't start One Tap UI: ${exception.localizedMessage}")
+//          }
+//        }
+//        .addOnFailureListener(this) { exception ->
+//          Timber.e(exception.localizedMessage)
+//        }
+      val signInIntent = googleSignInClient.signInIntent
+      loginLauncher.launch(signInIntent)
     }
   }
 
@@ -160,6 +186,35 @@ class LoginActivity : BaseActivity() {
           Toast.makeText(this@LoginActivity, message, Toast.LENGTH_SHORT).show()
         }
       }
+    }
+  }
+
+  private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+    try {
+      val googleIdToken = completedTask.getResult(ApiException::class.java)?.idToken
+      if (googleIdToken != null) {
+        // Google ID 토큰을 사용해 Firebase 인증 자격 증명을 생성
+        val firebaseCredential = GoogleAuthProvider.getCredential(googleIdToken, null)
+        // 생성된 Firebase 인증 자격 증명을 사용하여 Firebase 에 로그인을 시도
+        Firebase.auth.signInWithCredential(firebaseCredential).addOnCompleteListener { task ->
+          if (task.isSuccessful) {
+            Firebase.auth.currentUser?.getIdToken(true)?.addOnCompleteListener { idTokenTask ->
+              if (idTokenTask.isSuccessful) {
+                idTokenTask.result?.token?.let { token ->
+                  idToken = token
+                  viewModel.login(idToken)
+                } ?: Timber.e("FirebaseIdToken is null.")
+              }
+            }
+          } else {
+            Timber.e(task.exception)
+          }
+        }
+      } else {
+        Timber.e("GoogleIdToken is null.")
+      }
+    } catch (exception: ApiException) {
+      Timber.e(exception.localizedMessage)
     }
   }
 }

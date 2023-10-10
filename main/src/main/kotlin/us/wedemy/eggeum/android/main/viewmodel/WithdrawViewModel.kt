49777d7f4d9c342917c627b5afafed95ae4f2393
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-10-10 20:34:41 +0900

    회원 탈퇴 플로우 구현

--- main/src/main/kotlin/us/wedemy/eggeum/android/main/viewmodel/WithdrawViewModel.kt
+++ main/src/main/kotlin/us/wedemy/eggeum/android/main/viewmodel/WithdrawViewModel.kt
@@ -9,17 +9,37 @@ package us.wedemy.eggeum.android.main.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import us.wedemy.eggeum.android.common.util.getMutableStateFlow
+import us.wedemy.eggeum.android.domain.usecase.WithdrawUseCase
 
 @HiltViewModel
-class WithdrawViewModel @Inject constructor(savedStateHandle: SavedStateHandle) : ViewModel() {
-  private val _agreeToNotification = savedStateHandle.getMutableStateFlow(KEY_AGREE_TO_NOTIFICATION, false)
-  val agreeToNotification = _agreeToNotification.asStateFlow()
+class WithdrawViewModel @Inject constructor(
+  private val withdrawUseCase: WithdrawUseCase,
+  savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+  private val _agreeToWithdraw = savedStateHandle.getMutableStateFlow(KEY_AGREE_TO_NOTIFICATION, false)
+  val agreeToWithdraw = _agreeToWithdraw.asStateFlow()
 
-  fun setCbWithdrawAgreeToNotification() {
-    _agreeToNotification.value = !agreeToNotification.value
+  private val _navigateToLoginEvent = MutableSharedFlow<Unit>(replay = 1)
+  val navigateToLoginEvent: SharedFlow<Unit> = _navigateToLoginEvent.asSharedFlow()
+
+  fun setAgreeToWithdraw() {
+    _agreeToWithdraw.value = !agreeToWithdraw.value
+  }
+
+  fun withdraw() {
+    viewModelScope.launch {
+      withdrawUseCase.execute()
+      // TODO API 호출이 성공적으로 수행되었을때 화면 이동 이벤트 방출
+      _navigateToLoginEvent.emit(Unit)
+    }
   }
 
   private companion object {

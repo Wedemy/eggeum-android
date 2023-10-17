fe87d72c990becfd0f69ed6fdd81b720ffefbca1
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-10-17 14:42:38 +0900

    공지 아이템 확장 축소 Animation 방식 변경
    Custom Animation 이 아닌 Notify 함수의 내장 애니메이션을 사용하도록 변경

--- main/src/main/kotlin/us/wedemy/eggeum/android/main/ui/adapter/viewholder/NoticeListItemViewHolder.kt
+++ main/src/main/kotlin/us/wedemy/eggeum/android/main/ui/adapter/viewholder/NoticeListItemViewHolder.kt
@@ -18,70 +18,8 @@ class NoticeListItemViewHolder(val binding: ItemNoticeListBinding) : RecyclerVie
       tvNoticeTitle.text = notice.title
       tvNoticeDate.text = notice.date
       tvNoticeDescription.text = notice.description
-//      clNotice.setOnClickListener {
-//        val show = toggleLayout(!notice.isExpanded, ivNoticeExpand, llLayoutExpand)
-//        notice.isExpanded = show
-//      }
-      clNotice.setOnClickListener {
-        toggleContent(binding.llLayoutExpand)
-      }
+      ivNoticeExpand.rotation = if (notice.isExpanded) 180f else 0f
+      llLayoutExpand.visibility = if (notice.isExpanded) View.VISIBLE else View.GONE
     }
   }
-
-//  private fun toggleLayout(isExpanded: Boolean, view: View, layoutExpand: LinearLayout): Boolean {
-//    ToggleAnimation.toggleArrow(view, isExpanded)
-//    if (isExpanded) {
-//      ToggleAnimation.expand(layoutExpand)
-//    } else {
-//      ToggleAnimation.collapse(layoutExpand)
-//    }
-//    return isExpanded
-//  }
-
-  private fun toggleContent(hiddenView: View) {
-    if (hiddenView.visibility == View.VISIBLE) {
-      // collapseView(hiddenView)
-      with(binding) {
-        llLayoutExpand.visibility = View.GONE
-        ivNoticeExpand.animate().setDuration(200).rotation(180f)
-      }
-    } else {
-      // expandView(hiddenView)
-      with(binding) {
-        llLayoutExpand.visibility = View.VISIBLE
-        ivNoticeExpand.animate().setDuration(200).rotation(0f)
-      }
-    }
-  }
-
-//  private fun expandView(view: View) {
-//    view.measure(RecyclerView.LayoutParams.MATCH_PARENT, RecyclerView.LayoutParams.WRAP_CONTENT)
-//    val targetHeight = view.measuredHeight
-//
-//    view.layoutParams.height = 0
-//    view.visibility = View.VISIBLE
-//    val animator = ValueAnimator.ofInt(0, targetHeight)
-//    animator.addUpdateListener { animation ->
-//      view.layoutParams.height = (animation.animatedValue as Int)
-//      view.requestLayout()
-//    }
-//    animator.duration = 300
-//    animator.start()
-//  }
-//
-//  private fun collapseView(view: View) {
-//    val initialHeight = view.measuredHeight
-//    val animator = ValueAnimator.ofInt(initialHeight, 0)
-//    animator.addUpdateListener { animation ->
-//      view.layoutParams.height = (animation.animatedValue as Int)
-//      view.requestLayout()
-//    }
-//    animator.addListener(object : AnimatorListenerAdapter() {
-//      override fun onAnimationEnd(animation: Animator) {
-//        view.visibility = View.GONE
-//      }
-//    })
-//    animator.duration = 300
-//    animator.start()
-//  }
 }

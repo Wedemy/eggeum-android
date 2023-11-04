ad9710ada69c52f1551ed2c57a494d78403ec816
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-11-04 17:29:07 +0900

    Login API 누락된 404 Exception 처리 추가
    Ktor 에서 Retrofit 으로 migration 하면서 누락된 처리 추가

--- data/src/main/kotlin/us/wedemy/eggeum/android/data/datasource/login/LoginRemoteDataSourceImpl.kt
+++ data/src/main/kotlin/us/wedemy/eggeum/android/data/datasource/login/LoginRemoteDataSourceImpl.kt
@@ -7,20 +7,60 @@
 
 package us.wedemy.eggeum.android.data.datasource.login
 
+import java.net.UnknownHostException
 import javax.inject.Inject
+import retrofit2.HttpException
+import timber.log.Timber
+import us.wedemy.eggeum.android.data.extensions.toAlertMessage
 import us.wedemy.eggeum.android.data.model.login.LoginRequest
 import us.wedemy.eggeum.android.data.model.login.LoginResponse
 import us.wedemy.eggeum.android.data.model.login.SignUpRequest
 import us.wedemy.eggeum.android.data.model.login.SignUpResponse
 import us.wedemy.eggeum.android.data.service.LoginService
+import us.wedemy.eggeum.android.data.util.ExceptionWrapper
 import us.wedemy.eggeum.android.data.util.safeRequest
+import us.wedemy.eggeum.android.domain.util.LoginApiResponseNotFound
 
 public class LoginRemoteDataSourceImpl @Inject constructor(
   private val service: LoginService,
 ) : LoginRemoteDataSource {
   public override suspend fun login(loginRequest: LoginRequest): LoginResponse? {
-    return safeRequest {
-      service.login(loginRequest)
+    try {
+      val response = service.login(loginRequest)
+      if (response.isSuccessful) {
+        return response.body()
+      }
+      else if (response.code() == 404) {
+        throw LoginApiResponseNotFound
+      }
+      else {
+        val errorBody = response.errorBody()?.string() ?: "Unknown error"
+        Timber.d(Exception(errorBody))
+        throw ExceptionWrapper(
+          statusCode = response.code(),
+          message = Exception(errorBody).toAlertMessage(),
+          cause = Exception(errorBody),
+        )
+      }
+    } catch (exception: HttpException) {
+      Timber.d(exception)
+      throw ExceptionWrapper(
+        statusCode = exception.code(),
+        message = exception.response()?.errorBody()?.string() ?: exception.message(),
+        cause = exception,
+      )
+    } catch (exception: UnknownHostException) {
+      Timber.d(exception)
+      throw ExceptionWrapper(
+        message = exception.toAlertMessage(),
+        cause = exception
+      )
+    } catch (exception: Exception) {
+      Timber.d(exception)
+      throw ExceptionWrapper(
+        message = exception.toAlertMessage(),
+        cause = exception
+      )
     }
   }
 

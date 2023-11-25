ced382c0075d8ee64fabce3e00662210b8f70dc7
kymjaehong <dwby.jr@gmail.com>
2023-11-25 13:35:39 +0900

    edit: imageview -> spinner (#147)

--- update-cafe/src/main/kotlin/us/wedemy/eggeum/android/updatecafe/ui/adapter/viewholder/CafeMenuViewHolder.kt
+++ update-cafe/src/main/kotlin/us/wedemy/eggeum/android/updatecafe/ui/adapter/viewholder/CafeMenuViewHolder.kt
@@ -7,34 +7,21 @@
 
 package us.wedemy.eggeum.android.updatecafe.ui.adapter.viewholder
 
-import android.app.Dialog
-import android.util.Log
-import android.view.KeyEvent
-import android.view.MotionEvent
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
 import java.text.NumberFormat
 import java.util.Locale
-import us.wedemy.eggeum.android.common.extension.layoutInflater
 import us.wedemy.eggeum.android.common.ui.BaseViewHolder
-import us.wedemy.eggeum.android.updatecafe.databinding.ItemEditBtnBinding
 import us.wedemy.eggeum.android.updatecafe.databinding.ItemUpdateCafeMenuBinding
 import us.wedemy.eggeum.android.updatecafe.ui.item.CafeMenuItem
 
 class CafeMenuViewHolder(binding: ItemUpdateCafeMenuBinding) : BaseViewHolder<CafeMenuItem, ItemUpdateCafeMenuBinding>(binding) {
-  private val dialog = Dialog(binding.root.context)
 
   override fun bind(item: CafeMenuItem) {
     binding.apply {
       tvCafeMenuName.text = item.name
       tvCafeMenuPrice.text = formatPrice(item.price)
 
-      imgBtn.setOnClickListener {
+      spinnerBtn.setOnClickListener {
         // TODO 수정 및 삭제 창
-        val btn = ItemEditBtnBinding.inflate(root.context.layoutInflater)
-        dialog.setContentView(btn.root)
-        dialog.setCancelable(true)
-        dialog.show()
       }
     }
   }

a559b8fad2aad795f77274c4021ce461d68b0c6f
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-12-21 17:51:58 +0900

    카페 이미지 상세화면 systembar 색상 변경 분기 처리

--- main/src/main/kotlin/us/wedemy/eggeum/android/main/ui/MainActivity.kt
+++ main/src/main/kotlin/us/wedemy/eggeum/android/main/ui/MainActivity.kt
@@ -8,9 +8,12 @@
 package us.wedemy.eggeum.android.main.ui
 
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.WindowInsetsController
 import androidx.activity.SystemBarStyle
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
@@ -41,6 +44,7 @@ class MainActivity : BaseActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     initNavigation()
+    setupDestinationChangeListener()
   }
 
   private fun initNavigation() {
@@ -52,6 +56,43 @@ class MainActivity : BaseActivity() {
           R.id.fragment_home, R.id.fragment_search, R.id.fragment_my_account -> View.VISIBLE
           else -> View.GONE
         }
+      }
+    }
+  }
+
+  private fun setupDestinationChangeListener() {
+    navController?.addOnDestinationChangedListener { _, destination, _ ->
+      when (destination.id) {
+        R.id.fragment_cafe_image_detail -> {
+          val systemBarColor = ContextCompat.getColor(this, us.wedemy.eggeum.android.design.R.color.muted_900)
+          updateSystemBars(systemBarColor)
+        }
+
+        else -> {
+          updateSystemBars(Color.TRANSPARENT)
+        }
+      }
+    }
+  }
+
+  private fun updateSystemBars(color: Int) {
+    window.statusBarColor = color
+    window.navigationBarColor = color
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+
+      val controller = window.insetsController
+      if (color == ContextCompat.getColor(this, us.wedemy.eggeum.android.design.R.color.muted_900)) {
+        controller?.setSystemBarsAppearance(0, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
+      } else {
+        controller?.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
+      }
+    } else {
+      @Suppress("DEPRECATION")
+      if (color == Color.BLACK) {
+        window.decorView.systemUiVisibility = 0
+      } else {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
       }
     }
   }

c23027d731cc99c0601eebf5a0ff1d8e0f392b55
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-12-21 19:40:38 +0900

    카메 이미지 상세화면 사진 인덱스 추가

--- main/src/main/kotlin/us/wedemy/eggeum/android/main/ui/search/CafeImageDetailFragment.kt
+++ main/src/main/kotlin/us/wedemy/eggeum/android/main/ui/search/CafeImageDetailFragment.kt
@@ -7,12 +7,19 @@
 
 package us.wedemy.eggeum.android.main.ui.search
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.view.View
 import android.widget.PopupMenu
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import us.wedemy.eggeum.android.common.extension.repeatOnStarted
 import us.wedemy.eggeum.android.common.ui.BaseFragment
 import us.wedemy.eggeum.android.main.R
 import us.wedemy.eggeum.android.main.databinding.FragmentCafeImageDetailBinding
@@ -20,7 +27,6 @@ import us.wedemy.eggeum.android.main.ui.adapter.CafeImageDetailAdapter
 import us.wedemy.eggeum.android.main.viewmodel.CafeImageDetailViewModel
 
 // TODO 툴바에 currentPosition 표기
-// statusBar, navigationBar 검은색으로
 @AndroidEntryPoint
 class CafeImageDetailFragment : BaseFragment<FragmentCafeImageDetailBinding>() {
 
@@ -36,12 +42,13 @@ class CafeImageDetailFragment : BaseFragment<FragmentCafeImageDetailBinding>() {
     super.onViewCreated(view, savedInstanceState)
     initView()
     initListener()
+    initObserver()
   }
 
   private fun initView() {
     binding.vpCafeImageDetail.apply {
       adapter = cafeImageDetailAdapter
-      setCurrentItem(viewModel.currentPosition, false)
+      setCurrentItem(viewModel.currentPosition.value, false)
     }
   }
 
@@ -64,24 +71,47 @@ class CafeImageDetailFragment : BaseFragment<FragmentCafeImageDetailBinding>() {
           if (it.itemId == R.id.cafe_image_report) {
             // TODO 카페 사진 신고하기 다이얼로그 구현
             true
-          }
-          else false
+          } else false
         }
       }
 
       ivCafeImageDetailNext.setOnClickListener {
         val currentItem = vpCafeImageDetail.currentItem
         vpCafeImageDetail.setCurrentItem(currentItem + 1, true)
+        viewModel.updateCurrentPosition(currentItem + 1)
       }
 
       ivCafeImageDetailPrev.setOnClickListener {
         val currentItem = vpCafeImageDetail.currentItem
         if (currentItem > 0) {
           vpCafeImageDetail.setCurrentItem(currentItem - 1, true)
+          viewModel.updateCurrentPosition(currentItem - 1)
         } else {
           // 이미지가 존재해야 해당 화면에 접근할 수 있으므로 imageUrlList 의 사이즈는 0 이 될 수 없음
           val lastIndex = (vpCafeImageDetail.adapter?.itemCount ?: 0) / viewModel.cafeImages.files.size - 1
           vpCafeImageDetail.setCurrentItem(lastIndex * viewModel.cafeImages.files.size, true)
+          viewModel.updateCurrentPosition(lastIndex * viewModel.cafeImages.files.size)
+        }
+      }
+    }
+  }
+
+  private fun initObserver() {
+    repeatOnStarted {
+      launch {
+        viewModel.currentPosition.collect { index ->
+          val text = "${index + 1} / ${viewModel.cafeImages.files.size}"
+          val spannableString = SpannableString(text)
+
+          val slashIndex = text.indexOf("/")
+
+          val whiteSpan = ForegroundColorSpan(Color.WHITE)
+          spannableString.setSpan(whiteSpan, 0, slashIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+          val graySpan = ForegroundColorSpan(ContextCompat.getColor(requireContext(), us.wedemy.eggeum.android.design.R.color.gray_500))
+          spannableString.setSpan(graySpan, slashIndex, text.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+          binding.tvCafeImageDetailImageIndex.text = spannableString
         }
       }
     }

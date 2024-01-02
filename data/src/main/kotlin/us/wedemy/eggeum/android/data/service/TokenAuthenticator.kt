dffe0f8fdb6d4b12f38ccbab875713a72197b007
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-01-02 11:06:56 +0900

    Authenticator 함수 내부 구현 변경
    401 로 떨어질 경우 refresh 토큰을 헤더에 담아 호출하는 가장 간단한 형태로 변경

--- data/src/main/kotlin/us/wedemy/eggeum/android/data/service/TokenAuthenticator.kt
+++ data/src/main/kotlin/us/wedemy/eggeum/android/data/service/TokenAuthenticator.kt
@@ -1,43 +1,23 @@
 package us.wedemy.eggeum.android.data.service
 
+import javax.inject.Inject
 import kotlinx.coroutines.runBlocking
 import okhttp3.Authenticator
 import okhttp3.Request
 import okhttp3.Response
 import okhttp3.Route
-import javax.inject.Inject
 import us.wedemy.eggeum.android.data.datastore.TokenDataStoreProvider
 
 public class TokenAuthenticator @Inject constructor(
   private val dataStoreProvider: TokenDataStoreProvider,
 ) : Authenticator {
 
-  override fun authenticate(route: Route?, response: Response): Request? {
-    val accessToken = runBlocking {
-      dataStoreProvider.getAccessToken()
+  override fun authenticate(route: Route?, response: Response): Request {
+    synchronized(this) {
+      val newAccessToken = runBlocking { dataStoreProvider.getRefreshToken() }
+      return newRequestWithAccessToken(response.request, newAccessToken)
     }
-
-    if (hasNotAccessTokenOnResponse(response)) {
-      synchronized(this) {
-        val newAccessToken = runBlocking {
-          dataStoreProvider.getAccessToken()
-        }
-        if (accessToken != newAccessToken) {
-          return newRequestWithAccessToken(response.request, newAccessToken)
-        }
-
-        val refreshToken = runBlocking {
-          dataStoreProvider.getRefreshToken()
-        }
-        return newRequestWithAccessToken(response.request, refreshToken)
-      }
-    }
-
-    return null
   }
-
-  private fun hasNotAccessTokenOnResponse(response: Response): Boolean =
-    response.header("Authorization") == null
 
   private fun newRequestWithAccessToken(request: Request, accessToken: String): Request =
     request.newBuilder()

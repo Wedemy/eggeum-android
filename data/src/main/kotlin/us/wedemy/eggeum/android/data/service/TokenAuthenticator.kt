8e95d4640aa5d588e2eaae17492d7b33625b7ab4
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-01-02 12:25:18 +0900

    RefreshTokenExpiredException 추가

--- data/src/main/kotlin/us/wedemy/eggeum/android/data/service/TokenAuthenticator.kt
+++ data/src/main/kotlin/us/wedemy/eggeum/android/data/service/TokenAuthenticator.kt
@@ -6,16 +6,32 @@ import okhttp3.Authenticator
 import okhttp3.Request
 import okhttp3.Response
 import okhttp3.Route
+import retrofit2.HttpException
 import us.wedemy.eggeum.android.data.datastore.TokenDataStoreProvider
+import us.wedemy.eggeum.android.domain.util.RefreshTokenExpiredException
 
+@Suppress("TooGenericExceptionCaught")
 public class TokenAuthenticator @Inject constructor(
   private val dataStoreProvider: TokenDataStoreProvider,
 ) : Authenticator {
 
   override fun authenticate(route: Route?, response: Response): Request {
     synchronized(this) {
-      val newAccessToken = runBlocking { dataStoreProvider.getRefreshToken() }
-      return newRequestWithAccessToken(response.request, newAccessToken)
+      try {
+        val newAccessToken = runBlocking { dataStoreProvider.getRefreshToken() }
+        return newRequestWithAccessToken(response.request, newAccessToken)
+      } catch (e: Exception) {
+        when(e) {
+          is HttpException -> {
+            if (e.code() == 401) {
+              throw RefreshTokenExpiredException
+            } else {
+              throw e
+            }
+          }
+          else -> throw e
+        }
+      }
     }
   }
 

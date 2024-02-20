0c43de736ede24b446b9e7e929be8ea62b71d868
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-02-20 22:26:01 +0900

    release debug 앱 분리

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,8 @@
 
 @file:Suppress("UnstableApiUsage", "INLINE_FROM_HIGHER_PLATFORM")
 
+import java.util.Properties
+
 plugins {
   eggeum("android-application")
   eggeum("android-hilt")
@@ -16,6 +18,36 @@ plugins {
 
 android {
   namespace = "us.wedemy.eggeum.android"
+
+  signingConfigs {
+    create("release") {
+      val propertiesFile = rootProject.file("keystore.properties")
+      val properties = Properties()
+      properties.load(propertiesFile.inputStream())
+      storeFile = file(properties["STORE_FILE"] as String)
+      storePassword = properties["STORE_PASSWORD"] as String
+      keyAlias = properties["KEY_ALIAS"] as String
+      keyPassword = properties["KEY_PASSWORD"] as String
+    }
+  }
+
+  buildTypes {
+    getByName("debug") {
+      isDebuggable = true
+      applicationIdSuffix = ".dev"
+      manifestPlaceholders += mapOf(
+        "appName" to "@string/app_name_dev",
+      )
+    }
+
+    getByName("release") {
+      isDebuggable = false
+      signingConfig = signingConfigs.getByName("release")
+      manifestPlaceholders += mapOf(
+        "appName" to "@string/app_name",
+      )
+    }
+  }
 
   buildFeatures {
     viewBinding = true

dc37f228b91b2c6caafc94e2118885d7d2261de9
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-04-02 21:33:09 +0900

    누락된 파일 헤더 추가 및 중복된 파일 헤더 삭제

--- app/src/main/kotlin/us/wedemy/eggeum/android/IntroActivity.kt
+++ app/src/main/kotlin/us/wedemy/eggeum/android/IntroActivity.kt
@@ -5,13 +5,6 @@
  * Please see full license: https://github.com/Wedemy/eggeum-android/blob/main/LICENSE
  */
 
-/*
- * Designed and developed by Wedemy 2023.
- *
- * Licensed under the MIT.
- * Please see full license: https://github.com/Wedemy/eggeum-android/blob/main/LICENSE
- */
-
 package us.wedemy.eggeum.android
 
 import android.graphics.Color
--- app/src/main/kotlin/us/wedemy/eggeum/android/initialize/FirebaseCrashlyticsInitializer.kt
+++ app/src/main/kotlin/us/wedemy/eggeum/android/initialize/FirebaseCrashlyticsInitializer.kt
@@ -1,3 +1,10 @@
+/*
+ * Designed and developed by Wedemy 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/Wedemy/eggeum-android/blob/main/LICENSE
+ */
+
 package us.wedemy.eggeum.android.initialize
 
 import android.content.Context

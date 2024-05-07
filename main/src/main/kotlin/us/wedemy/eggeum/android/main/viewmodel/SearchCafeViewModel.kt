f81cf3516bd3d52d2421cb60d082dbd5f5eb9465
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-05-07 21:07:21 +0900

    chore: 구현 완료한 TODO 제거

--- main/src/main/kotlin/us/wedemy/eggeum/android/main/viewmodel/SearchCafeViewModel.kt
+++ main/src/main/kotlin/us/wedemy/eggeum/android/main/viewmodel/SearchCafeViewModel.kt
@@ -29,7 +29,6 @@ import us.wedemy.eggeum.android.domain.usecase.DeleteRecentSearchPlaceUseCase
 import us.wedemy.eggeum.android.domain.usecase.GetSearchPlaceListUseCase
 import us.wedemy.eggeum.android.domain.usecase.InsertRecentSearchPlaceUseCase
 
-// TODO 검색 결과 아이템과 최근 검색 결과 아이템의 디자인이 달라야함
 @HiltViewModel
 class SearchCafeViewModel @Inject constructor(
   getSearchPlaceListUseCase: GetSearchPlaceListUseCase,

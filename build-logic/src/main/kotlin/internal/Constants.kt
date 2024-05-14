c8380028fd2f196d88342eb34f8a77d1e7d5408f
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-05-15 07:04:37 +0900

    chore: 이끔 정식 출시를 위한 app version up

--- build-logic/src/main/kotlin/internal/Constants.kt
+++ build-logic/src/main/kotlin/internal/Constants.kt
@@ -11,8 +11,8 @@ internal object ApplicationConstants {
   const val MinSdk = 24
   const val TargetSdk = 34
   const val CompileSdk = 34
-  const val VersionCode = 8
-  const val VersionName = "0.1.1"
+  const val VersionCode = 9
+  const val VersionName = "0.1.2"
   val JavaVersion = org.gradle.api.JavaVersion.VERSION_17
   const val JavaVersionAsInt = 17
 }
